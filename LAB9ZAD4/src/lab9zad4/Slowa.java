ec5a3d5d636953f8890340d648689dbd08752af3
XxX <XxX@XxX-Komputer>
2018-05-15 20:40:50 +0200

    Zadanie 4

---
+++ LAB9ZAD4/src/lab9zad4/Slowa.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab9zad4;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author XxX
+ */
+public class Slowa {
+
+    public void Elo(HashMap<String, String> slownik) {
+
+        slownik.put("byc", "be");
+        slownik.put("otrzymac", "get");
+        slownik.put("dzien", "day");
+        slownik.put("umowa", "deal");
+        slownik.put("obiad", "dinner");
+        slownik.put("obowiazek", "duty");
+        slownik.put("miec", "have");
+        slownik.put("sloik", "jar");
+        slownik.put("praca", "job");
+        slownik.put("dodawac", "add");
+        slownik.put("wiek", "age");
+        slownik.put("tata", "father");
+        slownik.put("mama", "mother");
+        slownik.put("syn", "son");
+        slownik.put("wujek", "uncle");
+        slownik.put("samochod", "car");
+        slownik.put("woda", "water");
+        slownik.put("stol", "table");
+        slownik.put("telefon", "mobile phone");
+        slownik.put("buty", "shoes");
+
+    }
+
+}
