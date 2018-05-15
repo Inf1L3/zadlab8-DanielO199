c4f7978d6827d8416ccc53ad13f466faa4f0d381
XxX <XxX@XxX-Komputer>
2018-05-15 20:46:06 +0200

    Zadanie 4

--- LAB9ZAD4/src/lab9zad4/LAB9ZAD4.java
+++ LAB9ZAD4/src/lab9zad4/LAB9ZAD4.java
@@ -6,30 +6,6 @@ public class LAB9ZAD4 {
 /*SPUSHOWANE DO ZLEGO REPOZYTORIUM*/
     
     public static void main(String[] args) {
-        
-        HashMap<String, String> slownik = new HashMap<>();
-        Scanner input=new Scanner(System.in);
-        Slowa slowa1=new Slowa();
-        slowa1.Elo(slownik);
-        String a=" ";
-        
-        while(!a.equals("koniec"))
-           {
-             
-              System.out.println("Podaj słowo ktore chcesz przetlumaczyc:");
-              a = input.nextLine();
-              
-               
-               
-              if(!slownik.containsKey(a))
-              {
-                  System.out.println("Takiego slowa nie ma w slowniku ");
-             
-              }
-              else
-                    System.out.println(slownik.get(a));
-               System.out.println("Jesli chcesz zakonczyc napisz koniec");
-          } 
       
 }
 }
